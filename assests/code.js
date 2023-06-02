a64f259da59fc9da1e55969748d46812a72a21ea
TaysWeb <snelltararobin@gmail.com>
2023-06-02 17:01:07 +0200

    js started

---
+++ assests/code.js
@@ -0,0 +1,20 @@
+let task = [];
+
+  // Use of Date.now() method
+  var date = Date(Date.now());
+ 
+  //Converting the number of a millisecond in the date string
+
+  let createdDate = date.toString();
+  console.log(currentDate)
+
+function addPerson (){
+    let tasks = {
+        id: task.indexOf() ,
+        name: document.getElementById('insert'),
+        
+    }
+    task.push(tasks);
+    console.log(task)
+
+}

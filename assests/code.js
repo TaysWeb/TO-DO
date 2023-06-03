3da75b195c950f2ef99fba3b90683ed59870971a
TaysWeb <snelltararobin@gmail.com>
2023-06-03 15:01:46 +0200

    redo

--- assests/code.js
+++ assests/code.js
@@ -1,20 +1,26 @@
-let task = [];
-
-  // Use of Date.now() method
-  var date = Date(Date.now());
- 
-  //Converting the number of a millisecond in the date string
-
-  let createdDate = date.toString();
-  console.log(currentDate)
-
-function addPerson (){
-    let tasks = {
-        id: task.indexOf() ,
-        name: document.getElementById('insert'),
-        
-    }
-    task.push(tasks);
-    console.log(task)
-
+// getting from the HTML
+const taskInput = document.getElementById("insert") ;
+const addBtn = document.getElementById("add");
+const taskList = document.getElementById("display") ;
+//adding the event listener to button
+addBtn.addEventListener('click', addTask) ;
+//load task from Local Storage when the page loads  - from the HTML
+document.addEventListener('DOMContentLoaded', loadToDo) ;
+// function to add the to-do 
+function addTask() {
+  // gets my task from the input 
+   const itemTask = taskInput.value.trim() ;
+   // then return the task if theres none
+   if (itemTask === ' ') {
+     return
+   }
+   // creating a list item from Task 
+   const listItem = document.createElement('li');
+   listItem.textContent = itemTask ;  
+//append the list item to the \tasklist
+taskList.appendChild(listItem) ;
+// save tasks to local storage
+saveTask();
+// to clear the input 
+taskInput.value = " " ;
 }
